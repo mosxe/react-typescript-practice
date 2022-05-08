2bd0e69b82ccc9754e31e8d7e40d40d4a51dd23f
Kirill.Alekseev <kirill.alekseev@x5.ru>
2022-05-08 23:52:32 +0300

    start new application

---
+++ src/store/index.ts
@@ -0,0 +1,4 @@
+﻿import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+export const store = createStore({}, applyMiddleware(thunk));
---
+++ src/store/reducers/useReducer.ts
@@ -0,0 +1,32 @@
+﻿import { UserState, UserAction, UserActionTypes } from '../../types/todo';
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null
+};
+
+export const useReducer = (state = initialState, action: UserAction): UserState => {
+    switch (action.type) {
+      case UserActionTypes.FETCH_USERS: 
+        return {
+          loading: true,
+          users: [],
+          error: null
+        };
+      case UserActionTypes.FETCH_USERS_SUCCESS: 
+        return {
+          loading: false,
+          users: action.payload,
+          error: null
+        };
+      case UserActionTypes.FETCH_USERS_ERROR: 
+        return {
+          loading: false,
+          users: [],
+          error: action.payload
+        };
+      default:
+        return state;
+    }
+}
